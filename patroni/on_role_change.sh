7d0ebdcf942e411cfde866dcfaf13a6c1b4f7fc1
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-07-30 22:24:29 +0300

    add callback fix

--- patroni/on_role_change.sh
+++ patroni/on_role_change.sh
@@ -9,6 +9,7 @@ TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 API_URL=https://kubernetes.default.svc.cluster.local/api/v1
 
 # get all masters
+# get code -s -o -I -L -w "%{http_code}"
 ROLE_LABEL_MASTER=$(curl -s --cacert ${CA_BUNDLE} \
    -H "Authorization: Bearer ${TOKEN}" \
    ${API_URL}/namespaces/${NAMESPACE}/pods\?labelSelector=role\=master,app\=pg-postgres)
